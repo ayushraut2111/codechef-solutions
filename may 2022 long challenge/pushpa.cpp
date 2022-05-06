8b2a16f49cf4386bfaaa519226712cd48ec3140d
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-06 21:58:58 +0530

    pushpa

---
+++ may 2022 long challenge/pushpa.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int pushpa(int a[],int n)
+{
+    map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+        mp[a[i]]++;
+    }
+    int ans=INT_MIN;
+    for(auto it=mp.begin();it!=mp.end();it++)
+    {
+        ans=max(ans,it->first+(it->second-1));
+    }
+    return ans;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n;
+       cin>>n;
+       int a[n];
+       for(int i=0;i<n;i++)
+       {
+       cin>>a[i];
+       }
+       cout<<pushpa(a,n)<<endl;
+    }
+}
